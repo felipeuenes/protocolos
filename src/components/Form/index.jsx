eb2f8d6e5cd6ca2dea17c9f8e6ce033dd7efa99c
Felipe Uenes <uenesdesigner@gmail.com>
2023-06-29 14:52:46 -0300

    add images

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -19,20 +19,39 @@ export function Form(){
             console.log(data)
 
             const doc = new jsPDF();
-            doc.text("Protocolo de atendimento", 70, 30);
-            doc.text(`Nome da pessoa: ${data.nome} `, 30, 50);
-            doc.text(`__________________________________`, 75, 51);
-            doc.text(`Nome da empresa: ${data.empresa} `, 30, 60);
-            doc.text(`__________________________________`, 78, 61);
-            doc.text(`Data: ${data.data} `, 30, 75);
-            doc.text(`__________`, 43, 76);
+            doc.text("Protocolo de atendimento", 70, 34);
+            doc.text(`Nome da pessoa: ${data.nome} `, 25, 50);
+            doc.text(`__________________________________`, 70, 51);
+            doc.text(`Nome da empresa: ${data.empresa} `, 25, 60);
+            doc.text(`__________________________________`, 73, 61);
+            doc.text(`Data: ${data.data} `, 25, 75);
+            doc.text(`__________`, 38, 76);
             doc.text(`Numero do protocolo: ${data.servico}${data.data} `, 95, 75);
             doc.text(`__________`, 150, 76);
-            doc.text(`Observações: ${data.obs}  `, 30, 100);
+            doc.text(`Observações: ${data.obs}  `, 25, 100);
+            
+
+            // doc.addImage('./timbre2023.png',0 , 0, 100, 100)
+            const imgBaixo = new Image();
+            imgBaixo.src = './timbre-baixo.png'
+            
+
+            const img = new Image();
+            img.src = './timbre2023.png';
+            
+            // Esperar o carregamento completo da imagem
+
+            img.onload = function () {
+           
+
+            // Adicionar a imagem ao documento PDF
+            doc.addImage(img, 'JPEG', 10, 3, 230, 22);
+            doc.addImage(imgBaixo, 'JPEG', 0, 125, 210, 20);
+           
+
             doc.save("protocolo.pdf");
-
-            doc.addImage('./timbre2023',0 , 0)
-
+        }
+          
 
             // Carregue a imagem do cabeçalho
            
